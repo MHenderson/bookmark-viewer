60ec59bd4a6a8a3f477783a8d959d32942063d0c
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-04-14 20:14:08 +0100

    use magick to manipulate images

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(fs)
+library(magick)
 library(shiny)
 library(tidyverse)
 
@@ -13,6 +14,24 @@ N <- nrow(X)
 
 shinyServer(function(input, output) {
 
+  image_front <- reactive({
+    X %>%
+      filter(bookmark == req(input$bookmark)) %>%
+      slice(1) %>%
+      pull(image_path) %>%
+      image_read() %>%
+      image_write(tempfile(fileext='jpg'), format = 'jpg')
+  })
+
+  image_back <- reactive({
+    X %>%
+      filter(bookmark == req(input$bookmark)) %>%
+      slice(2) %>%
+      pull(image_path) %>%
+      image_read() %>%
+      image_write(tempfile(fileext='jpg'), format = 'jpg')
+  })
+
   output$bookmark <- renderUI({
     selectInput(
       inputId = "bookmark",
@@ -22,21 +41,25 @@ shinyServer(function(input, output) {
   })
 
   output$image_front <- renderImage({
+
     list(
-      src = X %>% filter(bookmark == req(input$bookmark)) %>% slice(1) %>% pull(image_path),
-      contentType = 'image/jpeg',
+      src = image_front(),
+      contentType = "image/jpeg",
       alt = "This is alternate text",
       height = 800
     )
-  }, deleteFile = FALSE)
+
+  })
 
   output$image_back <- renderImage({
+
     list(
-      src = X %>% filter(bookmark == req(input$bookmark)) %>% slice(2) %>% pull(image_path),
+      src = image_back(),
       contentType = 'image/jpeg',
       alt = "This is alternate text",
       height = 800
     )
-  }, deleteFile = FALSE)
+
+  })
 
 })
