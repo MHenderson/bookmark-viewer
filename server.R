3e2aac78f6b9ab3176117f3ac134de21b2eb6a5b
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-04-14 15:29:16 +0100

    show front and back. dropdown menu.

--- server.R
+++ server.R
@@ -3,19 +3,39 @@ library(shiny)
 library(tidyverse)
 
 X <- tibble(
-  image_path = dir_ls("bookmarks/images", recurse = TRUE, type = "file")
-)
+  image_path = dir_ls("bookmarks/images",recurse = TRUE, type = "file")
+) %>%
+  mutate(
+    bookmark = gsub("bookmarks/images/", "", dirname(image_path))
+  )
 
 N <- nrow(X)
 
 shinyServer(function(input, output) {
 
-  output$image <- renderImage({
+  output$bookmark <- renderUI({
+    selectInput(
+      inputId = "bookmark",
+      label = "",
+      choices = unique(X$bookmark)
+    )
+  })
+
+  output$image_front <- renderImage({
     list(
-              src = X %>% slice((input$action %% N) + 1) %>% pull(image_path),
+      src = X %>% filter(bookmark == input$bookmark) %>% slice(1) %>% pull(image_path),
       contentType = 'image/jpeg',
-              alt = "This is alternate text",
-              height = 800
+      alt = "This is alternate text",
+      height = 400
+    )
+  }, deleteFile = FALSE)
+
+  output$image_back <- renderImage({
+    list(
+      src = X %>% filter(bookmark == input$bookmark) %>% slice(2) %>% pull(image_path),
+      contentType = 'image/jpeg',
+      alt = "This is alternate text",
+      height = 400
     )
   }, deleteFile = FALSE)
 
--- ui.R
+++ ui.R
@@ -1,13 +1,28 @@
+library(bootstraplib)
 library(shiny)
+
+bs_theme_new()
 
 shinyUI(
   fluidPage(
 
+    bootstrap(),
+
     titlePanel(""),
 
     fluidRow(
-      plotOutput("image"),
-      actionButton("action", label = "Next")
+      column(
+        width = 2,
+        uiOutput("bookmark")
+      ),
+      column(
+        width = 5,
+        wellPanel(plotOutput("image_front"))
+      ),
+      column(
+        width = 5,
+        wellPanel(plotOutput("image_back"))
+      )
     )
 
  ))
